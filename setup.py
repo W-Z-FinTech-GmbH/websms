bd70ce2eb8d544d97bfe960fad02d3e3a2908390
Piotr Lukaj <piotr.lukaj@gmail.com>
2022-02-22 16:17:00 +0100

    Add `websms` codebase

---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='websms',
+    version='1.0.0',
+    description=(
+        'Python library that allows to send messages using WebSMS platform.'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/W-Z-FinTech-GmbH/websms',
+    author='W&Z FinTech GmbH',
+    author_email='dk@ownly.de',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+    ],
+    keywords='sms websms message text phone',
+    py_modules=['websms'],
+    install_requires=['requests >= 2.25'],
+    test_suite='tests',
+    tests_require=['mock'],
+)
